1ebf9a7812f4d5d2d33f1ff8043c978e754fb2d0
Roman Belov <rnbelov96@gmail.com>
2020-03-29 10:55:26 +0300

    Page title changed

--- webpack.prod.js
+++ webpack.prod.js
@@ -61,7 +61,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       inject: false,
       template: require('html-webpack-template'),
-      title: 'Document',
+      title: 'Autocomplete App',
       mobile: true,
       lang: 'en-US',
       appMountId: 'root',
