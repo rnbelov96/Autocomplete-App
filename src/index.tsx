6f03e258a1452a8eb28278cf1e34cd0c0724a84e
Roman Belov <rnbelov96@gmail.com>
2021-09-06 17:40:07 +0300

    Возвращает удаленный импорт стилей

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
+import './style/main.sass';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
